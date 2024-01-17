1b035281d6906d47b18f3c795b102c9816a0f502
MoulOut <mmoulinlima@outlook.com>
2024-01-17 15:33:34 -0300

    refactor

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 alert('Welcome to the game of the secret number');
-let secretNumber = 5;
-let guessNumber,tries = 1;
+let secretNumber = parseInt(Math.random() * 100 + 1);
+let guessNumber,
+  tries = 1;
 
 while (secretNumber != guessNumber) {
-  guessNumber = prompt('Type a number betwen 1 and 10:');
+  guessNumber = prompt('Type a number betwen 1 and 100:');
   if (secretNumber == guessNumber) {
-    alert(`Awesome, you guessed the right number: ${secretNumber}. Number of tries:${tries}`);
+    break;
   } else {
     if (guessNumber > secretNumber) {
       alert(`Secret number is lower than ${guessNumber}`);
@@ -16,3 +17,6 @@ while (secretNumber != guessNumber) {
     alert('Try again');
   }
 }
+alert(
+  `Awesome, you guessed the right number: ${secretNumber}. Number of tries:${tries}`
+);
