322ddd19bcf950437a2a04246464da3129de2633
MoulOut <mmoulinlima@outlook.com>
2024-02-02 08:08:16 -0300

    changes to proposal

--- Course#/app.js
+++
@@ -1,8 +0,0 @@
-function goToPage() {
-  window.location.href = 'https://www.youtube.com/watch?v=mAKsZ26SabQ';
-}
-
-function buttonChange() {
-  document.querySelector('button#yes2').innerHTML = 'Yes!';
-  document.querySelector('button#yes2').removeAttribute('disabled');
-}
