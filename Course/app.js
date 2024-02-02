42933db928e45364eb197268aa5e4eb2c1b07774
MoulOut <mmoulinlima@outlook.com>
2024-02-02 08:16:09 -0300

    proposal project

---
+++ Course/app.js
@@ -0,0 +1,8 @@
+function goToPage() {
+  window.location.href = 'https://www.youtube.com/watch?v=mAKsZ26SabQ';
+}
+
+function buttonChange() {
+  document.querySelector('button#yes2').innerHTML = 'Yes!';
+  document.querySelector('button#yes2').removeAttribute('disabled');
+}
